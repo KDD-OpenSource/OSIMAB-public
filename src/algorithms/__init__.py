8785c76d55ed6d0d89674461a4159321b1e3dcf0
kahanor <30160323+kahanor@users.noreply.github.com>
2019-09-19 16:28:31 +0200

    Add aae, aae_diff and autoencoder_diff to __init__.py

--- src/algorithms/__init__.py
+++ src/algorithms/__init__.py
@@ -1,12 +1,18 @@
 from .dagmm import DAGMM
 from .donut import Donut
 from .autoencoder import AutoEncoder
+from .autoencoder_diff import AutoEncoderDiff
+from .aae_diff import AdversarialAutoEncoderDiff
+from .aae import AdversarialAutoEncoder
 from .lstm_ad import LSTMAD
 from .lstm_enc_dec_axl import LSTMED
 from .rnn_ebm import RecurrentEBM
 
 __all__ = [
     'AutoEncoder',
+    'AutoEncoderDiff',
+    'AdversarialAutoEncoder',
+    'AdversarialAutoEncoderDiff',
     'DAGMM',
     'Donut',
     'LSTMAD',
