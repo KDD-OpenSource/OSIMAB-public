4af318454793301c03b29c8fa1554ec9b499081c
kahanor <30160323+kahanor@users.noreply.github.com>
2020-04-13 10:16:10 +0200

    Move detectors to osimab for ease of use

--- osimab.py
+++ osimab.py
@@ -2,7 +2,16 @@ import numpy as np
 
 from src.datasets import OSIMABDataset
 from src.evaluation import Evaluator
-from main import detectors
+from src.algorithms import AutoEncoder
+from src.algorithms import LSTMEDP
+from src.algorithms import LSTMED
+
+
+def detectors(seed):
+    standard_epochs = 40
+    dets = [LSTMED(num_epochs=standard_epochs, seed=seed)]
+
+    return sorted(dets, key=lambda x: x.framework)
 
 
 def main():
@@ -10,8 +19,8 @@ def main():
 
 
 def evaluate_osimab():
-    seed = np.random.randint(np.iinfo(np.uint32).max, size=1, dtype=np.uint32)[0]
-    datasets = [OSIMABDataset()]
+    seed = 0
+    datasets = [OSIMABDataset(file_name='N_F2_INC_5_30min.csv')]
     evaluator = Evaluator(datasets, detectors, seed=seed)
     evaluator.evaluate()
     result = evaluator.benchmarks()
