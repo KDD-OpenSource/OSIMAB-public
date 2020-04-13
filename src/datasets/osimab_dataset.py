c505ad20ed9ae4b625ff8bd6150f2ad3d2cf6b9f
kahanor <30160323+kahanor@users.noreply.github.com>
2020-04-13 10:13:51 +0200

    Change osimab dataset to standardize data

--- src/datasets/osimab_dataset.py
+++ src/datasets/osimab_dataset.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 from .real_datasets import RealDataset
 
 
 class OSIMABDataset(RealDataset):
-    def __init__(self):
+    def __init__(self, file_name=None):
+        if file_name is None:
+            file_name = 'osimab-data.csv'
         super().__init__(
-            name='OSIMAB Dataset', raw_path='osimab-data', file_name='osimab-data.csv'
+            name='OSIMAB Dataset', raw_path='osimab-data', file_name=file_name
         )
 
     def load(self):
@@ -18,7 +21,10 @@ class OSIMABDataset(RealDataset):
         df = pd.read_csv(self.processed_path)
         n_train = int(df.shape[0] * 0.7)
         train = df.iloc[:n_train]
+        scaler = StandardScaler()
+        train = pd.DataFrame(scaler.fit_transform(train), columns=train.columns)
         test = df.iloc[n_train:]
+        test = pd.DataFrame(scaler.transform(test), columns=test.columns)
         train_label = pd.Series(np.zeros(train.shape[0]))
         test_label = pd.Series(np.zeros(test.shape[0]))
         return (train, train_label), (test, test_label)
