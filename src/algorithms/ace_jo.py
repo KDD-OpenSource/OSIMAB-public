8285de021bdadb8e5903208a048d46c76385b506
Benedikt Boeing <boeing@bit.uni-bonn.de>
2020-09-03 19:04:11 +0200

    Making the temporary plots usable

--- src/algorithms/ace_jo.py
+++ src/algorithms/ace_jo.py
@@ -209,7 +209,6 @@ class AutoEncoderJO(Algorithm, PyTorchUtils):
                         :] = error_rhs
             self.prediction_details.update({'errors_mean_rhs': np.nanmean(lattice, axis=0).T})
 
-            import pdb; pdb.set_trace()
             import os
             import matplotlib.pyplot as plt
             os.chdir('tmp')
@@ -217,22 +216,32 @@ class AutoEncoderJO(Algorithm, PyTorchUtils):
             for channel in range(self.input_size):
                 self.encoding_details.update({f'channel_{channel}':
                     encodings[:,channel]})
-            
+
+            num_plots = 100
             encodings = encodings.reshape((encodings.shape[0],-1))
-            origDataTMP = sequences[:1000:10]
+            origDataTMP = sequences[:10*num_plots:10]
+            ymin_1 = 1.1*np.array(origDataTMP)[:,:,0].min()
+            ymin_2 = 1.1*np.array(origDataTMP)[:,:,1].min()
+            ymin_3 = 1.1*np.array(origDataTMP)[:,:,2].min()
+            ymax_1 = 1.1*np.array(origDataTMP)[:,:,0].max()
+            ymax_2 = 1.1*np.array(origDataTMP)[:,:,1].max()
+            ymax_3 = 1.1*np.array(origDataTMP)[:,:,2].max()
             i=0
-            for elem in encodings[np.random.choice(encodings.shape[0],100)]:
+            for elem in encodings[np.random.choice(encodings.shape[0],num_plots)]:
                 plt.plot(elem)
                 plt.savefig(f'rand_enc_{i}')
                 plt.close('all')
                 i = i+1
             i=0
-            for elem in encodings[:1000:10]:
-                fig, ax = plt.subplots(4,1)
+            for elem in encodings[:10*num_plots:10]:
+                fig, ax = plt.subplots(4,1, figsize =(15,10))
                 ax[0].plot(elem)
                 ax[1].plot(origDataTMP[i][:,0])
+                ax[1].set_ylim([ymin_1, ymax_1])
                 ax[2].plot(origDataTMP[i][:,1])
+                ax[2].set_ylim([ymin_2, ymax_2])
                 ax[3].plot(origDataTMP[i][:,2])
+                ax[3].set_ylim([ymin_3, ymax_3])
                 fig.savefig(f'enc_{i}')
                 plt.close('all')
                 i = i+1
@@ -240,7 +249,6 @@ class AutoEncoderJO(Algorithm, PyTorchUtils):
             import pdb; pdb.set_trace()
 
             self.encoding_details.update({'encodings': encodings})
-            
 
         return scores_lhs + scores_rhs
 
