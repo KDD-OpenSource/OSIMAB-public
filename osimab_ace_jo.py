a37f786e1a1886c38bfce4390bb1b32597974c39
Benedikt Boeing <boeing@bit.uni-bonn.de>
2020-09-03 14:14:05 +0200

    add new datafiles (parameters, no real code changes)

--- osimab_ace_jo.py
+++ osimab_ace_jo.py
@@ -12,12 +12,14 @@ import random
 
 
 def detectors(seed):
-    standard_epochs = 10
+    standard_epochs = 1
     dets = [AutoEncoderJO(num_epochs=standard_epochs,
         hidden_size1 = 5,
         hidden_size2 = 5,
         lr = 1e-5,
         sequence_length = 100,
+        #batch_size = 2,
+        sensor_specific = False,
         #train_max = 1000,
         seed=seed)]
 
@@ -30,7 +32,7 @@ def main():
 
 def evaluate_osimab_jo():
     #seed = random.randint(0,100)
-    seed = 2
+    seed = 4
     datasets = [
             #OSIMABDataset(file_name='OSIMAB_04_01_19_F6_ACC_S1.csv')
             #OSIMABDataset(file_name='OSIMAB_04_01_19_F6_INC_1.csv')
@@ -38,8 +40,23 @@ def evaluate_osimab_jo():
             #OSIMABDataset(file_name='OSIMAB_04_01_19_F6_WA_SO.csv')
             #OSIMABDataset(file_name='OSIMABData_04_01_19_F6_SG.csv')
             #OSIMABDataset(file_name='OSIMAB_full_NT_INC.csv')
-            OSIMABDataset(file_name='OSIMAB_full_NT_INC.csv')
-            #OSIMABDataset(file_name='OSIMAB_small_NT_WA.csv')
+            #OSIMABDataset(file_name='OSIMAB_full_NT_INC_1.csv')
+            #OSIMABDataset(file_name='OSIMAB_full_NT_WA.csv'),
+            #OSIMABDataset(file_name='OSIMAB_full_NT_ACC.csv'),
+            #OSIMABDataset(file_name='OSIMAB_full_NT_SG4.csv'),
+            #OSIMABDataset(file_name='OSIMAB_full_NT_SG8.csv')
+            OSIMABDataset(file_name='OSIMAB_mid_NT_INC.csv')
+            #OSIMABDataset(file_name='OSIMAB_mid_NT_INC_1.csv')
+            #OSIMABDataset(file_name='OSIMAB_mid_NT_WA.csv'),
+            #OSIMABDataset(file_name='OSIMAB_mid_NT_ACC.csv'),
+            #OSIMABDataset(file_name='OSIMAB_mid_NT_SG4.csv'),
+            #OSIMABDataset(file_name='OSIMAB_mid_NT_SG8.csv')
+            #OSIMABDataset(file_name='OSIMAB_small_NT_INC.csv')
+            #OSIMABDataset(file_name='OSIMAB_small_NT_INC_1.csv')
+            #OSIMABDataset(file_name='OSIMAB_small_NT_WA.csv'),
+            #OSIMABDataset(file_name='OSIMAB_small_NT_ACC.csv'),
+            #OSIMABDataset(file_name='OSIMAB_small_NT_SG4.csv'),
+            #OSIMABDataset(file_name='OSIMAB_small_NT_SG8.csv')
             ]
     evaluator = Evaluator(datasets, detectors, seed=seed)
     evaluator.evaluate()
