corrects calculation of alpha and beta (#31)
* corrects calculation of alpha and beta

* black linting

Co-authored-by: Benedikt Boeing <boeing@bit.uni-bonn.de>

src/algorithms/ace_jo.py
@@ -139,6 +139,8 @@ class AutoEncoderJO(Algorithm, PyTorchUtils):
         self.aed.train()
         alpha, beta = self.initTradeoff()
         for epoch in trange(self.num_epochs):
+            print(f"Alpha: {alpha}")
+            print(f"Beta: {beta}")
             epochLossLhs = 0
             epochLossRhs = 0
             latentSpace = []
@@ -152,23 +154,24 @@ class AutoEncoderJO(Algorithm, PyTorchUtils):
                 epochLossRhs += loss2
                 (alpha * loss1 + beta * loss2).backward()
                 optimizer.step()
-            alpha, beta = self.updateTradeoff(alpha, beta, epoch)
+            alpha, beta = self.updateTradeoff(alpha, beta, epoch + 1)
             latentSpace = np.vstack(
                 list(map(lambda x: x.detach().numpy(), latentSpace))
             )
             self.printIntermedResults(epoch, epochLossLhs, epochLossRhs, latentSpace)
 
     def initTradeoff(self):
-        alpha = 1
-        beta = 1e-3
-        # beta = 0
+        alpha_frac = 1
+        beta_frac = 1 / (2 ** (self.num_epochs - 1))
+        alpha = alpha_frac / (alpha_frac + beta_frac)
+        beta = beta_frac / (alpha_frac + beta_frac)
         return alpha, beta
 
     def updateTradeoff(self, alpha, beta, epoch):
-        # alpha/=2
-        # beta*=2
-        alpha = 1 - epoch / self.num_epochs
-        beta = epoch / self.num_epochs
+        alpha_frac = 1 / (2 ** epoch)
+        beta_frac = 1 / (2 ** (self.num_epochs - (epoch + 1)))
+        alpha = alpha_frac / (alpha_frac + beta_frac)
+        beta = beta_frac / (alpha_frac + beta_frac)
         return alpha, beta
 
     def calcLosses(self, ts_batch, output):
@@ -200,7 +203,10 @@ class AutoEncoderJO(Algorithm, PyTorchUtils):
         sqr_err = error ** 2
         sum_sqr_err = sqr_err.sum(1)
         root_sum_sqr_err = torch.sqrt(sum_sqr_err)
+        sqr_sum_sqr_err = sum_sqr_err ** 2
         return root_sum_sqr_err
+        # return sqr_sum_sqr_err
+        # return sum_sqr_err
 
     def corr_loss(self, yhat, y):
         subclassLength = self.hidden_size1
@@ -361,6 +367,23 @@ class AutoEncoderJO(Algorithm, PyTorchUtils):
         )
 
         if self.details:
+            self.prediction_details.update(
+                {"anomaly_values": self.anomaly_values.values.T}
+            )
+            self.prediction_details.update(
+                {
+                    "anomaly_values_lhs": (
+                        scoresSensors_lhs.T > self.anomaly_thresholds_lhs
+                    ).T
+                }
+            )
+            self.prediction_details.update(
+                {
+                    "anomaly_values_rhs": (
+                        scoresSensors_lhs.T > self.anomaly_thresholds_rhs
+                    ).T
+                }
+            )
             self.prediction_details.update({"scoresSensors_lhs": scoresSensors_lhs})
             self.prediction_details.update({"scoresSensors_rhs": scoresSensors_rhs})
             self.prediction_details.update({"scores_lhs": scores_lhs})